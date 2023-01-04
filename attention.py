e9554f5461f24fddae7dd702308d98bc2420d0c2
junhsss <junhsssr@gmail.com>
2023-01-04 09:16:15 +0000

    Vectorize multi-head attentions

--- attention.py
+++ attention.py
@@ -1,33 +1,38 @@
 import torch
-from einops import einsum, pack, unpack
+from einops import einsum, rearrange, unpack
 from einops.layers.torch import EinMix
 
 x = torch.randn(196, 1000)
 num_heads = 5
 head_dim = 20
+inner_dim = num_heads * head_dim
 
-outs = []
-for i in range(num_heads):
-    to_qkv = EinMix(
-        "n d_in -> n d_out",
-        weight_shape="d_in d_out",
-        bias_shape="d_out",
-        d_in=1000,
-        d_out=3 * head_dim,
-    )
 
-    query, key, value = unpack(
-        to_qkv(x),
-        [[head_dim], [head_dim], [head_dim]],
-        "n *",
-    )
+to_qkv = EinMix(
+    "n d_in -> n d_out",
+    weight_shape="d_in d_out",
+    bias_shape="d_out",
+    d_in=1000,
+    d_out=3 * inner_dim,
+)
 
-    dots = einsum(query, key, "q d, k d -> q k") * head_dim**-0.5
+query, key, value = unpack(
+    to_qkv(x),
+    [[inner_dim], [inner_dim], [inner_dim]],
+    "n *",
+)
 
-    attention = dots.softmax(dim=-1)
+query, key, value = map(
+    lambda x: rearrange(x, "n (h d) -> h n d", h=num_heads),
+    (query, key, value),
+)
 
-    out = einsum(attention, value, "q k, k d -> q d")
+dots = einsum(query, key, "... q d, ... k d -> ... q k") * head_dim**-0.5
 
-    outs.append(out)
+attention = dots.softmax(dim=-1)
 
-outs, packed_shape = pack(outs, "n *")
+out = einsum(attention, value, "... q k, ... k d -> ... q d")
+
+out = rearrange(out, "h n d -> n (h d)")
+
+print(out.shape)
